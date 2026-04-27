11dee47f97062698ddd839ef1b7f4074bbb70145
schyolokov <154579030+schyolokov@users.noreply.github.com>
2026-04-27 15:17:58 +0500

    Delete templates directory

--- templates/fire.js
+++
@@ -1,6 +0,0 @@
-function fireTemplate(data) {
-  return {
-    ru: `Уволить ${data.fio} с должности ${data.position} ${data.date}`,
-    kz: `${data.fio} ${data.date} күні ${data.position} қызметінен босатылсын`
-  };
-}
--- templates/vacation.js
+++
@@ -1,6 +0,0 @@
-function vacationTemplate(data) {
-  return {
-    ru: `Предоставить отпуск ${data.fio} с ${data.date}`,
-    kz: `${data.fio} үшін ${data.date} бастап демалыс берілсін`
-  };
-}
